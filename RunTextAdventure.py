4297750032972e09911c39499645594857b5d150
Harrison Went <wentharrison@gmail.com>
2019-01-09 22:25:58 +0000

    Added help command Added not understood message for invalid commands Added input formatting and correction Added inventory Finished search room command Started item interaction

--- RunTextAdventure.py
+++ RunTextAdventure.py
@@ -1,8 +1,10 @@
 import os
+
 def clear():
     os.system('cls')
 
 def run_adventure():
+
     game_complete = False
 
     #Load default room
@@ -10,48 +12,101 @@ def run_adventure():
     current_room = f.readline()
     f.close()
 
+    my_inventory_of_keys = []
+
     while not game_complete:
+
         #open current room
         f = open_room(current_room)
+        searched_Room = False
 
         #read room description
         print(f.readline())
 
+        #get room items
         item_count = int(f.readline())
         room_items = get_items(f,item_count)
 
-        user_input = get_input()
+        #actions while in room
+        moving_out_of_room = False
+        while not moving_out_of_room:
+            user_input = get_input()
+            used_command = False
 
-        if user_input == "searchroom":
-            for val in room_items:
-                print("You can see " + val["ItemName"])
-        #if(user_input == "interact with item"):
-            #if( item wins game):
-                #game_complete = True
+            #Search room
+            if user_input == "searchroom":
+                used_command = True
+                searched_Room = True
+                for val in room_items:
+                    print("You can see " + val["ItemName"])
 
-        #if(pivkup item name):
-            #add item to inventory
-        #if(use item)
-            #todo add lines for after interacting with item such as "you destroyed the reactor"
+            #Check inventory
+            if user_input == "checkinventory":
+                used_command = True
+                if len(my_inventory_of_keys) == 0:
+                    print("There are no items in your inventory!")
+                else:
+                    print("ITEMS IN YOUR INVENTORY:")
+                    for a in my_inventory_of_keys:
+                        print(a["KeyName"])
 
-        #if(user input == help):
-            #print commands avaliable
+            #Interact with items
+            # todo get item name and search if contains that pickup word
+            for a in room_items:
+                if user_input in clean_string(a["InteractPhrases"]):
+                    used_command = True
+                    if not searched_Room:
+                        print("You haven't searched the room yet!")
+                    else:
+                        print(a["ItemDescription"])
+                        if a["IsKeyFor"] != "null":
+                            print("you pick it up and put it in your backpack")
+                            my_inventory_of_keys.append({"KeyName": a["ItemName"]})
+                        if a["EndsGame"] == "true":
+                            game_complete = True
 
-        #if(userinput == open #### door):
-            #try to open door
-            #if requires key
-                #if have key
-                    #say used key
-                #else say require key
-            #else
-                #open room
+            #if(use item)
+                #todo add lines for after interacting with item such as "you destroyed the reactor"
+
+            #Help options
+            if(user_input.__contains__("help")):
+                used_command = True
+                print("AVALIABLE COMMANDS:")
+                print(": Check inventory")
+                if not searched_Room:
+                    print(": Search room")
+                else:
+                    for a in room_items:
+                        print(": ",a["InteractPhrases"]," :",a["ItemName"])
+
+            #command not understood
+            if not used_command:
+                print("I don't understand that command")
+
+            #todo open doors
+            #if(userinput == open #### door):
+                #try to open door
+                #if requires key
+                    #if have key
+                        #say used key
+                    #else say require key
+                #else
+                    #open room
+
+            input()
+            clear()
+        #prepare for next room
         f.close()
         clear()
 
-    exit()
-    #TODO default text such as "You have" + item name
+    #Finish game
+    f = open_room("Default")
+    f.readline()
+    print(f.readline())
+    input()
+    f.close()
 
-    #TODO storing picked up items
+    exit()
 
 def open_room(name):
     name = name.rstrip()
@@ -59,19 +114,31 @@ def open_room(name):
     return f
 
 def get_input():
-    user_input = input("What shall i do?")
+    user_input = input("What shall i do? (enter HELP for help)")
+    user_input = clean_string(user_input)
     return user_input
+
+def clean_string(input_string):
+    input_string = input_string.lower()
+    input_string = input_string.replace(' ', "")
+
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    temp = ""
+    for char in input_string:
+        if char not in punctuations:
+            temp = temp + char
+        input_string = temp
+    return input_string
 
 def get_items(f, item_count):
     a=0
     items = []
     while a < item_count:
-        itemName = f.readline()
-        itemDescription = f.readline()
-        isKeyFor = f.readline()
-        phrases = f.readline()
-        gameEnder = f.readline()
-        items.append({"ItemName" : itemName, "ItemDescription" : itemDescription,"IsKeyFor" : isKeyFor,"InteractPhrases" : phrases,"EndsGame" :gameEnder})
+        items.append({"ItemName" : f.readline().replace('\n', '').replace('\r', ''),
+                      "ItemDescription" : f.readline().replace('\n', '').replace('\r', ''),
+                      "IsKeyFor" : f.readline().replace('\n', '').replace('\r', ''),
+                      "InteractPhrases" : f.readline().replace('\n', '').replace('\r', ''),
+                      "EndsGame" : f.readline().replace('\n', '').replace('\r', '')})
         a+=1
     return items
 
